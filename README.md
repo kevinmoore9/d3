e19cb4416eaa7228453f9fa915c2a4c190771538
Kevin <kevinmoore9@users.noreply.github.com>
2017-07-27 14:59:06 -0700

    add README.md

---
+++ README.md
@@ -0,0 +1,10 @@
+# Overview
+* This project will use past pitcher data to analyze the result of their pitches
+  (hit, strike, ball, out) based on pitch type and location
+* Use hitter data to analyze pitch result (swinging strike, looking strike,
+  hit, out) based on pitch type and location
+* Create a visual representation of hot/cold zones and pitch type for each batter/pitcher
+* Allow users to see a visual representation of any pitcher/hitter matchup
+* This project will only evaluate data from the 2016 MLB season
+
+### Data on 2016 MLB season collected from MySportsFeeds
--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 const APIUtil = require("./api_util");
 
 // debugger
-let games = APIUtil.getGames("BOS");
-let plays = APIUtil.getPlayByPlay("20160427", "OAK", "DET")
-console.log(plays.responseJSON);
+// let games = APIUtil.getGames("BOS");
+// let plays = APIUtil.getPlayByPlay("20160427", "OAK", "DET")
+// console.log(plays.responseJSON);
+
+APIUtil.getGames("BOS").then(data => {
+  console.log(data.fullgameschedule.gameentry[0]);
+});
+
 
 // "https://api.mysportsfeeds.com/v1.1/pull/mlb/2016-regular/cumulative_player_stats.json?playerstats=AB,H,R,HR,ER"
-
-
-// game schedule: "https://api.mysportsfeeds.com/v1.1/pull/mlb/2016-regular/full_game_schedule.json?team=OAK"
-
-// play by play: "https://api.mysportsfeeds.com/v1.1/pull/mlb/2016-regular/game_playbyplay.json?gameid=20160427-OAK-DET"
